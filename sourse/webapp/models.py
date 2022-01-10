142417e2dec2be036194d2005c22cc74c65253f8
NiiazovMaksat <niyazov37@gmail.com>
2022-01-10 12:00:37 +0600

    добавлена модель, зарегистрирована в админке

--- sourse/webapp/models.py
+++ sourse/webapp/models.py
@@ -8,4 +8,12 @@ class Product(models.Model):
     description = models.TextField(max_length=2000, verbose_name="Описание")
     category = models.CharField(max_length=100, verbose_name="Категория", choices=STATUS_CHOICES, default='other')
     count = models.IntegerField(max_length=10, verbose_name="Остаток")
-    price = models.DecimalField(max_length=10, verbose_name="Цена")
+    price = models.DecimalField(decimal_places=2, max_digits=9, max_length=10, verbose_name="Цена")
+
+    def __str__(self):
+        return f'{self.pk}) {self.name} : {self.category} / {self.count} | {self.price}'
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
