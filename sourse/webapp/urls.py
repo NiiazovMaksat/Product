f0542ed850e13a8dca88295f39d8eb95e342f283
NiiazovMaksat <niyazov37@gmail.com>
2022-01-10 13:33:36 +0600

    добавлена миграция, доработан базовый шаблон, добавлена страница дитального просмотра. прописаны пути для детальног просмотра, добавлена Вьюшка для гет запроса изменения

--- sourse/webapp/urls.py
+++ sourse/webapp/urls.py
@@ -1,6 +1,10 @@
 
 from django.urls import path
 
-from webapp.views import main_page
+from webapp.views import main_page, view_page
+
 urlpatterns = [
-    path('', main_page, name="main")
+    path('', main_page, name="main"),
+    path('view/<int:pk>', view_page, name="view"),
+    path('edit/<int:pk>', edit_page, name="edit")
+]
--- sourse/webapp/views.py
+++ sourse/webapp/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from webapp.models import Product
 
 
 def main_page(request):
-    product = Product.objects.order_by('updated_at')
-    return render(request, 'main_page.html',{'product': product})
+    product = Product.objects.order_by('category', 'name')
+    return render(request, 'main_page.html', {'product': product})
+
+def view_page(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "view_page.html", context)
+
+def edit_page(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'GET':
+        form = Product(initial={
+            'name': product.name,
+            'description': product.description,
+            'category': product.category,
+            'count': product.count,
+            'price': product.price
+        })
+
+        return render(request, 'edit.html', {'product': product, 'form': form})
+    else:
+        pass
